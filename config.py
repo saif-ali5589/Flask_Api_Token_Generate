6f55c7da9a12def90a521f4487818d0af48dfff8
saif-ali5589 <56355020+saif-ali5589@users.noreply.github.com>
2021-05-23 17:00:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 POSTGRES = {
-    'user': 'postgres',
-    'pw': 'saif@123#',
-    'db': 'flask_api',
-    'host': 'localhost',
-    'port': '5432',
+    'user': '',
+    'pw': '',
+    'db': '',
+    'host': '',
+    'port': '',
 }
